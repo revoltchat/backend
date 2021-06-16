d2b17b64e7bcf3aaa60e8124b22e8b358227f1c1
Paul <paulmakles@gmail.com>
2021-06-16 19:36:59 +0100

    Docker: Publish to :latest tag when building.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source set_version.sh
 
-docker build -t revoltchat/server:${version} . &&
-    docker push revoltchat/server:${version}
+                  docker build -t revoltchat/server:${version} . &&
+docker tag revoltchat/server:${version} revoltchat/server:latest &&
+                        docker push revoltchat/server:${version} &&
+                            docker push revoltchat/server:latest
