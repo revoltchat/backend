edd8ef8a1d4499089a3a4505a46d8634cbd45001
Paul <paulmakles@gmail.com>
2021-07-06 12:21:40 +0100

    Patch max password length on rauth.

--- set_version.sh
+++ set_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export version=0.5.1-alpha.4
+export version=0.5.1-alpha.5
 echo "pub const VERSION: &str = \"${version}\";" > src/version.rs
--- src/version.rs
+++ src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "0.5.1-alpha.4";
+pub const VERSION: &str = "0.5.1-alpha.5";
