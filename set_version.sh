dd65b9f358346832e396543fcf095f30bb8de168
Paul <paulmakles@gmail.com>
2021-07-27 16:25:32 +0100

    Try make content match href.

--- set_version.sh
+++ set_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export version=0.5.1-alpha.12
+export version=0.5.1-alpha.13
 echo "pub const VERSION: &str = \"${version}\";" > src/version.rs
--- src/version.rs
+++ src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "0.5.1-alpha.12";
+pub const VERSION: &str = "0.5.1-alpha.13";
