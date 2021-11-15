68901ade5fe3287ba21fefc04db701c384798398
Zomatree <angelokontaxis@hotmail.com>
2021-11-15 16:20:03 +0000

    add sendable embeds

--- src/database/entities/microservice/january.rs
+++ src/database/entities/microservice/january.rs
@@ -3,6 +3,7 @@ use crate::util::{
     variables::JANUARY_URL,
     variables::MAX_EMBED_COUNT,
 };
+use crate::database::entities::microservice::autumn::File;
 use linkify::{LinkFinder, LinkKind};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
@@ -93,10 +94,27 @@ pub struct Metadata {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Text {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub icon_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub media: Option<File>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+	pub colour: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(tag = "type")]
 pub enum Embed {
     Website(Metadata),
     Image(Image),
+    Text(Text),
     None,
 }
 
