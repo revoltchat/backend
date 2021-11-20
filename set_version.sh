1303dbc8443ee430185a6e02290a42e1a4070d62
Paul <paulmakles@gmail.com>
2021-11-20 13:20:21 +0000

    chore: bump version to 0.5.3-alpha.9

--- set_version.sh
+++ set_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export version=0.5.3-alpha.8
+export version=0.5.3-alpha.9
 echo "pub const VERSION: &str = \"${version}\";" > src/version.rs
--- src/version.rs
+++ src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "0.5.3-alpha.8";
+pub const VERSION: &str = "0.5.3-alpha.9";
