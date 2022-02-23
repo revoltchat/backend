d45b32bce69815fcedbb23bb05e86ec2c010bde3
Paul Makles <paulmakles@gmail.com>
2022-02-23 14:34:29 +0000

    feat(roles create): consider member's ranking and allow specifying custom ranking on creation

--- src/routes/servers/roles_create.rs
+++ src/routes/servers/roles_create.rs
@@ -11,6 +11,7 @@ use validator::Validate;
 pub struct Data {
     #[validate(length(min = 1, max = 32))]
     name: String,
+    rank: Option<i64>,
 }
 
 #[post("/<target>/roles", data = "<data>")]
@@ -20,17 +21,31 @@ pub async fn req(db: &Db, user: User, target: Ref, data: Json<Data>) -> Result<V
         .map_err(|error| Error::FailedValidation { error })?;
 
     let server = target.as_server(db).await?;
-    perms(&user)
-        .server(&server)
+    let mut permissions = perms(&user).server(&server);
+
+    permissions
         .throw_permission(db, Permission::ManageRole)
         .await?;
 
+    let member_rank = permissions.get_member_rank();
+    let rank = if let Some(given_rank) = data.rank {
+        if given_rank <= member_rank.unwrap_or(i64::MIN) {
+            return Err(Error::NotElevated);
+        }
+
+        given_rank
+    } else {
+        member_rank.unwrap_or(0).saturating_add(1)
+    };
+
     let role = Role {
         name: data.name,
+        rank,
         ..Default::default()
     };
 
     Ok(json!({
-        "id": role.create(db, &server.id).await?
+        "id": role.create(db, &server.id).await?,
+        "role": role
     }))
 }
