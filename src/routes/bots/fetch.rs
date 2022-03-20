e245b272fa6729c3c8fbf330f7876ec96f97d39a
Paul Makles <paulmakles@gmail.com>
2022-03-20 18:47:04 +0000

    feat: update bot routes to match old spec

--- src/routes/bots/fetch.rs
+++ src/routes/bots/fetch.rs
@@ -3,13 +3,23 @@ use revolt_quark::{
     Db, Error, Ref, Result,
 };
 use rocket::serde::json::Json;
+use serde::Serialize;
+
+/// # Bot Response
+#[derive(Serialize, JsonSchema)]
+pub struct BotResponse {
+    /// Bot object
+    bot: Bot,
+    /// User object
+    user: User,
+}
 
 /// # Fetch Bot
 ///
 /// Fetch details of a bot you own by its id.
 #[openapi(tag = "Bots")]
 #[get("/<target>")]
-pub async fn fetch_bot(db: &Db, user: User, target: Ref) -> Result<Json<Bot>> {
+pub async fn fetch_bot(db: &Db, user: User, target: Ref) -> Result<Json<BotResponse>> {
     if user.bot.is_some() {
         return Err(Error::IsBot);
     }
@@ -19,5 +29,8 @@ pub async fn fetch_bot(db: &Db, user: User, target: Ref) -> Result<Json<Bot>> {
         return Err(Error::NotFound);
     }
 
-    Ok(Json(bot))
+    Ok(Json(BotResponse {
+        user: db.fetch_user(&bot.id).await?.foreign(),
+        bot,
+    }))
 }
