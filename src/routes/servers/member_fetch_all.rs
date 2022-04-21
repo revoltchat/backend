350bd9c0be2c577b4c6d3bf456b2400edc78d245
Paul Makles <paulmakles@gmail.com>
2022-04-21 18:16:09 +0100

    feat: add parameter to skip offline users on fetch

--- src/routes/servers/member_fetch_all.rs
+++ src/routes/servers/member_fetch_all.rs
@@ -6,7 +6,14 @@ use revolt_quark::{
 };
 
 use rocket::serde::json::Json;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+
+/// # Query Parameters
+#[derive(Deserialize, JsonSchema, FromForm)]
+pub struct OptionsFetchAllMembers {
+    /// Whether to exclude offline users
+    exclude_offline: Option<bool>,
+}
 
 /// # Member List
 ///
@@ -23,8 +30,13 @@ pub struct AllMemberResponse {
 ///
 /// Fetch all server members.
 #[openapi(tag = "Server Members")]
-#[get("/<target>/members")]
-pub async fn req(db: &Db, user: User, target: Ref) -> Result<Json<AllMemberResponse>> {
+#[get("/<target>/members?<options..>")]
+pub async fn req(
+    db: &Db,
+    user: User,
+    target: Ref,
+    options: OptionsFetchAllMembers,
+) -> Result<Json<AllMemberResponse>> {
     let server = target.as_server(db).await?;
     perms(&user).server(&server).calc(db).await?;
 
@@ -49,6 +61,13 @@ pub async fn req(db: &Db, user: User, target: Ref) -> Result<Json<AllMemberRespo
     // Ensure the lists match up exactly.
     members.sort_by(|a, b| a.id.user.cmp(&b.id.user));
     users.sort_by(|a, b| a.id.cmp(&b.id));
+
+    // Optionally, remove all offline user entries.
+    if let Some(true) = options.exclude_offline {
+        let mut iter = users.iter();
+        members.retain(|_| iter.next().unwrap().online.unwrap_or(false));
+        users.retain(|user| user.online.unwrap_or(false));
+    }
 
     Ok(Json(AllMemberResponse { members, users }))
 }
