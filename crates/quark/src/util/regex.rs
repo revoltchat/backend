cba8c4ef3b5683c679ce3ca6fd5a7a0929925b7d
Paul Makles <paulmakles@gmail.com>
2022-07-15 16:13:04 +0100

    fix: add case-insensitive flag to colour regex

--- crates/quark/src/util/regex.rs
+++ crates/quark/src/util/regex.rs
@@ -5,6 +5,9 @@ use regex::Regex;
 ///
 /// Allows the use of named colours, rgb(a), variables and all gradients.
 ///
+/// Flags:
+/// - Case-insensitive (`i`)
+///
 /// Source:
 /// ```regex
 /// VALUE = [a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+
@@ -14,5 +17,5 @@ use regex::Regex;
 /// ^(?:VALUE|(repeating-)?(linear|conic|radial)-gradient\((VALUE|ADDITIONAL_VALUE)STOP(,[ ]*(VALUE)STOP)+\))$
 /// ```
 pub static RE_COLOUR: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"^(?:[a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+|(repeating-)?(linear|conic|radial)-gradient\(([a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+|\d+deg)([ ]+(\d{1,3}%|0))?(,[ ]*([a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+)([ ]+(\d{1,3}%|0))?)+\))$").unwrap()
+    Regex::new(r"(?i)^(?:[a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+|(repeating-)?(linear|conic|radial)-gradient\(([a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+|\d+deg)([ ]+(\d{1,3}%|0))?(,[ ]*([a-z ]+|var\(--[a-z\d-]+\)|rgba?\([\d, ]+\)|#[a-f0-9]+)([ ]+(\d{1,3}%|0))?)+\))$").unwrap()
 });
