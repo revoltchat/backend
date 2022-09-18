f4dcd091ccf3d8a78f259d29c87193e3aaa47104
Paul Makles <paulmakles@gmail.com>
2022-09-18 12:12:31 +0100

    feat: add 'Focus' presence status

--- crates/quark/src/models/users/user.rs
+++ crates/quark/src/models/users/user.rs
@@ -32,9 +32,15 @@ pub struct Relationship {
 /// Presence status
 #[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
 pub enum Presence {
+    /// User is online
     Online,
+    /// User is not currently available
     Idle,
+    /// User is focusing / will only receive mentions
+    Focus,
+    /// User is busy / will not receive any notifications
     Busy,
+    /// User appears to be offline
     Invisible,
 }
 
