2710edb76b76ae7282236b2e62c082161c6b3a26
Paul Makles <me@insrt.uk>
2023-03-03 08:17:55 +0000

    feat(safety): add api route for fetching a report

--- crates/delta/src/routes/safety/mod.rs
+++ crates/delta/src/routes/safety/mod.rs
@@ -2,6 +2,7 @@ use revolt_rocket_okapi::revolt_okapi::openapi3::OpenApi;
 use rocket::Route;
 
 mod edit_report;
+mod fetch_report;
 mod fetch_reports;
 mod report_content;
 
@@ -10,9 +11,10 @@ mod fetch_snapshot;
 pub fn routes() -> (Vec<Route>, OpenApi) {
     openapi_get_routes_spec![
         // Reports
-        report_content::report_content,
-        fetch_reports::fetch_reports,
         edit_report::edit_report,
+        fetch_report::fetch_report,
+        fetch_reports::fetch_reports,
+        report_content::report_content,
         // Snapshots
         fetch_snapshot::fetch_snapshot
     ]
