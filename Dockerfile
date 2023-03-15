32a294a64a6d77c77221066a82287f42da5ac6d1
Tim Davis <contact@timdav.is>
2023-03-15 14:39:54 -0700

    feat: add support for arm64 (#142)
    - set explicit action permissions
    - make package owner dynamic so forks can test workflow
    - build multi platform images using cross compilation amd64 -> arm64
    - add a scope to buildx gha cache

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,27 @@
 # Build Stage
-FROM rustlang/rust:nightly-slim AS builder
+FROM --platform="${BUILDPLATFORM}" rustlang/rust:nightly-slim
 USER 0:0
 WORKDIR /home/rust/src
 
-# Install build requirements
-RUN apt-get update && apt-get install -y libssl-dev pkg-config
+ARG TARGETARCH
 
-# Build all crates
+# Install build requirements
+RUN dpkg --add-architecture "${TARGETARCH}"
+RUN apt-get update && \
+    apt-get install -y \
+    make \
+    pkg-config \
+    libssl-dev:"${TARGETARCH}"
+COPY scripts/build-image-layer.sh /tmp/
+RUN sh /tmp/build-image-layer.sh tools
+
+# Build all dependencies
 COPY Cargo.toml Cargo.lock ./
+COPY crates/bonfire/Cargo.toml ./crates/bonfire/
+COPY crates/delta/Cargo.toml ./crates/delta/
+COPY crates/quark/Cargo.toml ./crates/quark/
+RUN sh /tmp/build-image-layer.sh deps
+
+# Build all apps
 COPY crates ./crates
-RUN cargo build --locked --release
+RUN sh /tmp/build-image-layer.sh apps
---
+++ scripts/build-image-layer.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -eu
+
+case "${TARGETARCH}" in
+  "amd64")
+    LINKER_NAME="x86_64-linux-gnu-gcc"
+    LINKER_PACKAGE="gcc-x86-64-linux-gnu"
+    BUILD_TARGET="x86_64-unknown-linux-gnu" ;;
+  "arm64")
+    LINKER_NAME="aarch64-linux-gnu-gcc"
+    LINKER_PACKAGE="gcc-aarch64-linux-gnu"
+    BUILD_TARGET="aarch64-unknown-linux-gnu" ;;
+esac
+
+tools() {
+  apt-get install -y "${LINKER_PACKAGE}"
+  rustup target add "${BUILD_TARGET}"
+}
+
+deps() {
+  mkdir -p \
+    crates/bonfire/src \
+    crates/delta/src \
+    crates/quark/src
+  echo 'fn main() { panic!("stub"); }' |
+    tee crates/bonfire/src/main.rs |
+    tee crates/delta/src/main.rs
+  echo '' |
+    tee crates/quark/src/lib.rs
+  cargo build --locked --release --target "${BUILD_TARGET}"
+}
+
+apps() {
+  touch -am \
+    crates/bonfire/src/main.rs \
+    crates/delta/src/main.rs \
+    crates/quark/src/lib.rs
+  cargo build --locked --release --target "${BUILD_TARGET}"
+  mv target _target && mv _target/"${BUILD_TARGET}" target
+}
+
+export RUSTFLAGS="-C linker=${LINKER_NAME}"
+export PKG_CONFIG_ALLOW_CROSS="1"
+export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig"
+
+"$@"
