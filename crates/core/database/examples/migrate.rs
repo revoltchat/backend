d633cba630cba39fe15ce083946a816d67a9419a
Paul Makles <me@insrt.uk>
2023-04-22 12:12:23 +0100

    fix: migration example

--- crates/core/database/examples/migrate.rs
+++ crates/core/database/examples/migrate.rs
@@ -1,8 +1,7 @@
 use revolt_database::DatabaseInfo;
-use revolt_models::*;
 
 #[async_std::main]
 async fn main() {
-    let db = Database(DatabaseInfo::Auto.connect().await.unwrap());
+    let db = DatabaseInfo::Auto.connect().await.unwrap();
     db.migrate_database().await.unwrap();
 }
