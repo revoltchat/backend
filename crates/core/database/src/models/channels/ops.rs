f8648e5c1d2f6166c7d1cb5609ceda698aa0ddfb
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-04-28 12:38:59 +0800

    cleaned up and documented function list for channels

--- crates/core/database/src/models/channels/ops.rs
+++ crates/core/database/src/models/channels/ops.rs
@@ -1,29 +1,45 @@
-use crate::{revolt_result::Result, Channel, FieldsChannel, PartialChannel};
+use crate::{revolt_result::Result, Channel, FieldsChannel};
 use revolt_permissions::OverrideField;
 mod mongodb;
 mod reference;
 
 #[async_trait]
 pub trait AbstractChannels: Sync + Send {
-    async fn fetch_channel(&self, id: &str) -> Result<Channel>;
-    async fn fetch_channels<'a>(&self, ids: &'a [String]) -> Result<Vec<Channel>>;
+    /// Insert a new channel in the database
     async fn insert_channel(&self, channel: &Channel) -> Result<()>;
-    async fn update_channel(
-        &self,
-        id: &str,
-        channel: &PartialChannel,
-        remove: Vec<FieldsChannel>,
-    ) -> Result<()>;
-    async fn delete_channel(&self, channel: &Channel) -> Result<()>;
-    async fn find_direct_messages(&self, user_id: &str) -> Result<Vec<Channel>>;
-    async fn find_saved_messages_channel(&self, user_id: &str) -> Result<Channel>;
-    async fn find_direct_message_channel(&self, user_a: &str, user_b: &str) -> Result<Channel>;
+
+    /// Insert a a user to a group
     async fn add_user_to_group(&self, channel: &str, user: &str) -> Result<()>;
-    async fn remove_user_from_group(&self, channel: &str, user: &str) -> Result<()>;
+
+    /// Insert channel role permissions
     async fn set_channel_role_permission(
         &self,
         channel: &str,
         role: &str,
         permissions: OverrideField,
     ) -> Result<()>;
+
+    /// Fetch a channel from the database
+    async fn fetch_channel(&self, id: &str) -> Result<Channel>;
+
+    /// Fetch all channels from the database
+    async fn fetch_channels<'a>(&self, ids: &'a [String]) -> Result<Vec<Channel>>;
+
+    /// Fetch all direct messages for a user
+    async fn find_direct_messages(&self, user_id: &str) -> Result<Vec<Channel>>;
+
+    // Fetch saved messages channel
+    async fn find_saved_messages_channel(&self, user_id: &str) -> Result<Channel>;
+
+    // Fetch direct message channel (PMs)
+    async fn find_direct_message_channel(&self, user_a: &str, user_b: &str) -> Result<Channel>;
+
+    async fn update_channel(
+        &self,
+        id: &str,
+        channel: &Channel,
+        remove: Vec<FieldsChannel>,
+    ) -> Result<()>;
+
+    async fn remove_user_from_group(&self, channel: &str, user: &str) -> Result<()>;
 }
