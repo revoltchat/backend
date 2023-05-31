edfa8e5256c287e0e04e5d4a972011719a395201
Paul Makles <me@insrt.uk>
2023-05-31 13:06:16 +0100

    feat(core/models): add account strikes

---
+++ crates/core/models/src/v0/account_strikes.rs
@@ -0,0 +1,24 @@
+auto_derived!(
+    /// Account Strike
+    pub struct AccountStrike {
+        /// Strike Id
+        #[serde(rename = "_id")]
+        pub id: String,
+        /// User Id of reported user
+        pub user_id: String,
+
+        /// Attached reason
+        pub reason: String,
+    }
+);
+
+#[cfg(feature = "from_database")]
+impl From<revolt_database::AccountStrike> for AccountStrike {
+    fn from(value: revolt_database::AccountStrike) -> Self {
+        AccountStrike {
+            id: value.id,
+            user_id: value.user_id,
+            reason: value.reason,
+        }
+    }
+}
