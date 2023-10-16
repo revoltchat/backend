4d42fb74e7a843b62c52ed5f28023e816ca92dce
Paul Makles <me@insrt.uk>
2023-10-16 09:22:17 +0100

    fix(core): publish user settings event

--- crates/core/database/src/models/user_settings/model.rs
+++ crates/core/database/src/models/user_settings/model.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::Database;
+use crate::{events::client::EventV1, Database};
 
 use revolt_result::Result;
 
@@ -16,12 +16,12 @@ impl UserSettingsImpl for UserSettings {
     async fn set(self, db: &Database, user: &str) -> Result<()> {
         db.set_user_settings(user, &self).await?;
 
-        /* // TODO: EventV1::UserSettingsUpdate {
+        EventV1::UserSettingsUpdate {
             id: user.to_string(),
             update: self,
         }
         .private(user.to_string())
-        .await; */
+        .await;
 
         Ok(())
     }
