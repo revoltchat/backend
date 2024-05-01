015b49d77eff27243c813b48eeee6eb1ae60816c
Aeris011 <165663563+Aeris011@users.noreply.github.com>
2024-05-01 01:16:56 -0300

    test ci

--- scripts/build-image-layer.sh
+++ scripts/build-image-layer.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-if [ -z "$TARGETARCH" ]; then
-  :
-else
-  case "${TARGETARCH}" in
-    "amd64")
-      LINKER_NAME="x86_64-linux-gnu-gcc"
-      LINKER_PACKAGE="gcc-x86-64-linux-gnu"
-      BUILD_TARGET="x86_64-unknown-linux-gnu" ;;
-    "arm64")
-      LINKER_NAME="aarch64-linux-gnu-gcc"
-      LINKER_PACKAGE="gcc-aarch64-linux-gnu"
-      BUILD_TARGET="aarch64-unknown-linux-gnu" ;;
-  esac
-fi
+set -eu
+
+case "${TARGETARCH}" in
+  "amd64")
+    LINKER_NAME="x86_64-linux-gnu-gcc"
+    LINKER_PACKAGE="gcc-x86-64-linux-gnu"
+    BUILD_TARGET="x86_64-unknown-linux-gnu" ;;
+  "arm64")
+    LINKER_NAME="aarch64-linux-gnu-gcc"
+    LINKER_PACKAGE="gcc-aarch64-linux-gnu"
+    BUILD_TARGET="aarch64-unknown-linux-gnu" ;;
+esac
 
 tools() {
   apt-get install -y "${LINKER_PACKAGE}"
@@ -25,7 +23,6 @@ deps() {
     crates/bonfire/src \
     crates/delta/src \
     crates/quark/src \
-    crates/core/config/src \
     crates/core/database/src \
     crates/core/models/src \
     crates/core/permissions/src \
@@ -36,18 +33,12 @@ deps() {
     tee crates/delta/src/main.rs
   echo '' |
     tee crates/quark/src/lib.rs |
-    tee crates/core/config/src/lib.rs |
     tee crates/core/database/src/lib.rs |
     tee crates/core/models/src/lib.rs |
     tee crates/core/permissions/src/lib.rs |
     tee crates/core/presence/src/lib.rs |
     tee crates/core/result/src/lib.rs
-  
-  if [ -z "$TARGETARCH" ]; then
-    cargo build --locked --release
-  else
-    cargo build --locked --release --target "${BUILD_TARGET}"
-  fi
+  cargo build --locked --release --target "${BUILD_TARGET}"
 }
 
 apps() {
@@ -55,27 +46,17 @@ apps() {
     crates/bonfire/src/main.rs \
     crates/delta/src/main.rs \
     crates/quark/src/lib.rs \
-    crates/core/config/src/lib.rs \
     crates/core/database/src/lib.rs \
     crates/core/models/src/lib.rs \
     crates/core/permissions/src/lib.rs \
     crates/core/presence/src/lib.rs \
     crates/core/result/src/lib.rs
-  
-  if [ -z "$TARGETARCH" ]; then
-    cargo build --locked --release
-  else
-    cargo build --locked --release --target "${BUILD_TARGET}"
-    mv target _target && mv _target/"${BUILD_TARGET}" target
-  fi
+  cargo build --locked --release --target "${BUILD_TARGET}"
+  mv target _target && mv _target/"${BUILD_TARGET}" target
 }
 
-if [ -z "$TARGETARCH" ]; then
-  :
-else
-  export RUSTFLAGS="-C linker=${LINKER_NAME}"
-  export PKG_CONFIG_ALLOW_CROSS="1"
-  export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig"
-fi
+export RUSTFLAGS="-C linker=${LINKER_NAME}"
+export PKG_CONFIG_ALLOW_CROSS="1"
+export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig"
 
 "$@"
