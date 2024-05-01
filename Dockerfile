046dba38c62e64eaa699293fc8a88dcc4b4acfc7
Aeris011 <165663563+Aeris011@users.noreply.github.com>
2024-04-30 23:43:31 -0300

    test script stack

--- Dockerfile
+++ Dockerfile
@@ -7,24 +7,19 @@ ARG TARGETARCH
 
 # Install build requirements
 RUN dpkg --add-architecture "${TARGETARCH}"
-RUN apt-get update &&
+RUN apt-get update && \
     apt-get install -y \
-        make \
-        pkg-config \
-        libssl-dev:"${TARGETARCH}"
+    make \
+    pkg-config \
+    libssl-dev:"${TARGETARCH}"
 COPY scripts/build-image-layer.sh /tmp/
-RUN /bin/bash /tmp/build-image-layer.sh tools
+RUN sh /tmp/build-image-layer.sh tools
 
 # Build all dependencies
 COPY Cargo.toml Cargo.lock ./
 COPY crates/bonfire/Cargo.toml ./crates/bonfire/
 COPY crates/delta/Cargo.toml ./crates/delta/
 COPY crates/quark/Cargo.toml ./crates/quark/
-COPY crates/core/database/Cargo.toml ./crates/core/database/
-COPY crates/core/models/Cargo.toml ./crates/core/models/
-COPY crates/core/permissions/Cargo.toml ./crates/core/permissions/
-COPY crates/core/presence/Cargo.toml ./crates/core/presence/
-COPY crates/core/result/Cargo.toml ./crates/core/result/
 RUN sh /tmp/build-image-layer.sh deps
 
 # Build all apps
--- scripts/build-image-layer.sh
+++ scripts/build-image-layer.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-set -eu
-
-case "${TARGETARCH}" in
-  "amd64")
-    LINKER_NAME="x86_64-linux-gnu-gcc"
-    LINKER_PACKAGE="gcc-x86-64-linux-gnu"
-    BUILD_TARGET="x86_64-unknown-linux-gnu" ;;
-  "arm64")
-    LINKER_NAME="aarch64-linux-gnu-gcc"
-    LINKER_PACKAGE="gcc-aarch64-linux-gnu"
-    BUILD_TARGET="aarch64-unknown-linux-gnu" ;;
-esac
+if [ -z "$TARGETARCH" ]; then
+  :
+else
+  case "${TARGETARCH}" in
+    "amd64")
+      LINKER_NAME="x86_64-linux-gnu-gcc"
+      LINKER_PACKAGE="gcc-x86-64-linux-gnu"
+      BUILD_TARGET="x86_64-unknown-linux-gnu" ;;
+    "arm64")
+      LINKER_NAME="aarch64-linux-gnu-gcc"
+      LINKER_PACKAGE="gcc-aarch64-linux-gnu"
+      BUILD_TARGET="aarch64-unknown-linux-gnu" ;;
+  esac
+fi
 
 tools() {
   apt-get install -y "${LINKER_PACKAGE}"
@@ -23,6 +25,7 @@ deps() {
     crates/bonfire/src \
     crates/delta/src \
     crates/quark/src \
+    crates/core/config/src \
     crates/core/database/src \
     crates/core/models/src \
     crates/core/permissions/src \
@@ -33,12 +36,18 @@ deps() {
     tee crates/delta/src/main.rs
   echo '' |
     tee crates/quark/src/lib.rs |
+    tee crates/core/config/src/lib.rs |
     tee crates/core/database/src/lib.rs |
     tee crates/core/models/src/lib.rs |
     tee crates/core/permissions/src/lib.rs |
     tee crates/core/presence/src/lib.rs |
     tee crates/core/result/src/lib.rs
-  cargo build --locked --release --target "${BUILD_TARGET}"
+  
+  if [ -z "$TARGETARCH" ]; then
+    cargo build --locked --release
+  else
+    cargo build --locked --release --target "${BUILD_TARGET}"
+  fi
 }
 
 apps() {
@@ -46,17 +55,27 @@ apps() {
     crates/bonfire/src/main.rs \
     crates/delta/src/main.rs \
     crates/quark/src/lib.rs \
+    crates/core/config/src/lib.rs \
     crates/core/database/src/lib.rs \
     crates/core/models/src/lib.rs \
     crates/core/permissions/src/lib.rs \
     crates/core/presence/src/lib.rs \
     crates/core/result/src/lib.rs
-  cargo build --locked --release --target "${BUILD_TARGET}"
-  mv target _target && mv _target/"${BUILD_TARGET}" target
+  
+  if [ -z "$TARGETARCH" ]; then
+    cargo build --locked --release
+  else
+    cargo build --locked --release --target "${BUILD_TARGET}"
+    mv target _target && mv _target/"${BUILD_TARGET}" target
+  fi
 }
 
-export RUSTFLAGS="-C linker=${LINKER_NAME}"
-export PKG_CONFIG_ALLOW_CROSS="1"
-export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig"
+if [ -z "$TARGETARCH" ]; then
+  :
+else
+  export RUSTFLAGS="-C linker=${LINKER_NAME}"
+  export PKG_CONFIG_ALLOW_CROSS="1"
+  export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig"
+fi
 
 "$@"
