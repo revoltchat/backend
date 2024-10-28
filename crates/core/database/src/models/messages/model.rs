c16da74484a79984af97469a0f6d6dbfe779a25f
IAmTomahawkx <iamtomahawkx@gmail.com>
2024-10-28 11:03:18 -0700

    fix: don't silence all messages

--- crates/core/database/src/models/messages/model.rs
+++ crates/core/database/src/models/messages/model.rs
@@ -493,7 +493,7 @@ impl Message {
                             }
                             _ => vec![],
                         },
-                        false,
+                        self.has_suppressed_notifications(),
                     )],
                 },
             )
