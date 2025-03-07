9c3b2fb3648f6d345fa7b58e27522b3e747af310
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2025-03-07 22:39:04 +0000

    fix: allow message pinning and unpinning for DM-like channels
    Signed-off-by: Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>

--- crates/delta/src/routes/channels/message_pin.rs
+++ crates/delta/src/routes/channels/message_pin.rs
@@ -1,7 +1,4 @@
-use revolt_database::{
-    util::{permissions::DatabasePermissionQuery, reference::Reference},
-    Database, PartialMessage, SystemMessage, User, AMQP,
-};
+use revolt_database::{util::{permissions::DatabasePermissionQuery, reference::Reference}, Channel, Database, PartialMessage, SystemMessage, User, AMQP};
 use revolt_models::v0::MessageAuthor;
 use revolt_permissions::{calculate_channel_permissions, ChannelPermission};
 use revolt_result::{create_error, Result};
@@ -22,10 +19,12 @@ pub async fn message_pin(
 ) -> Result<EmptyResponse> {
     let channel = target.as_channel(db).await?;
 
-    let mut query = DatabasePermissionQuery::new(db, &user).channel(&channel);
-    calculate_channel_permissions(&mut query)
-        .await
-        .throw_if_lacking_channel_permission(ChannelPermission::ManageMessages)?;
+    if !matches!(channel, Channel::DirectMessage { .. } | Channel::Group { .. } | Channel::SavedMessages { .. }) {
+        let mut query = DatabasePermissionQuery::new(db, &user).channel(&channel);
+        calculate_channel_permissions(&mut query)
+            .await
+            .throw_if_lacking_channel_permission(ChannelPermission::ManageMessages)?;
+    }
 
     let mut message = msg.as_message_in_channel(db, channel.id()).await?;
 
