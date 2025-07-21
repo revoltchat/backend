14ea180683d53e79ba781ca46cd90760c13c8d9c
Zomatree <me@zomatree.live>
2025-07-22 00:08:33 +0100

    refactor: simplify scope struct macro

--- crates/core/database/src/util/oauth2/scopes.rs
+++ crates/core/database/src/util/oauth2/scopes.rs
@@ -5,23 +5,25 @@ pub struct OAuth2Scoped<Scope> {
 }
 
 pub trait OAuth2Scope {
-    const NAME: &'static str;
     const SCOPE: crate::OAuth2Scope;
+    const MODEL: revolt_models::v0::OAuth2Scope;
 }
 
 macro_rules! define_oauth2_scope {
-    ($struct_name:ident, $name:literal) => {
+    ($struct_name:ident) => {
         pub struct $struct_name;
 
         impl OAuth2Scope for $struct_name {
-            const NAME: &'static str = $name;
             const SCOPE: crate::OAuth2Scope = crate::OAuth2Scope::$struct_name;
+            const MODEL: revolt_models::v0::OAuth2Scope = revolt_models::v0::OAuth2Scope::$struct_name;
         }
     };
 }
 
-define_oauth2_scope!(ReadIdentify, "read:identify");
-define_oauth2_scope!(ReadServers, "read:servers");
-define_oauth2_scope!(WriteFiles, "write:files");
-define_oauth2_scope!(Events, "events");
-define_oauth2_scope!(Full, "full");
+// This must match the OAuth2Scope enum
+// TODO: automatically sync this
+define_oauth2_scope!(ReadIdentify);
+define_oauth2_scope!(ReadServers);
+define_oauth2_scope!(WriteFiles);
+define_oauth2_scope!(Events);
+define_oauth2_scope!(Full);
